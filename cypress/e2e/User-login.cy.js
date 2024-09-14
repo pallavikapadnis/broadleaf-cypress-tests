a53b0f0086f83e8e83923f684268b1b5008a1392
pallavikapadnis <96579750+pallavikapadnis@users.noreply.github.com>
2024-09-14 23:55:49 +0530

    Delete cypress/e2e/User-login.cy.js

--- cypress/e2e/User-login.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Broadleaf Commerce Demo User Login', () => {
-    it('should log in a user successfully', () => {
-        cy.loginWithFixture(); // Use custom command to log in with fixture data
-    });
-});
