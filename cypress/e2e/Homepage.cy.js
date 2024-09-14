a090396f03609c01a70dedcae49e4c8f9c82d0c7
pallavikapadnis <96579750+pallavikapadnis@users.noreply.github.com>
2024-09-14 23:55:38 +0530

    Delete cypress/e2e/Homepage.cy.js

--- cypress/e2e/Homepage.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Broad leaf Demo Homepage', () => {
-    it('should load the homepage successfully', () => {
-      cy.visit('https://demo.broadleafcommerce.org/');
-      cy.contains('HOT SAUCE AFICIONADO?'); // Check for a specific element on the homepage
-    });
-  });
